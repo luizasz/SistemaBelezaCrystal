4cbb5fac2c57b246fc7c85f5a65fdd652d1aa4a1
Brenda Izabeli <izabelibrenda817@gmail.com>
2025-10-15 17:43:32 -0400

    agendamento

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -1,7 +1,13 @@
 namespace SistemaBelezaCrystal.Models
 {
-    public class Agendamento 
+    public class Agendamento
     {
-        
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Horario { get; set; }
+        public int IdCliente { get; set; }
+        public int IdServico { get; set; }
+        public int IdFuncionario { get; set; }
+        public int IdStatus { get; set; }
     }
 }
--- Models/AgendamentoDAO.cs
+++ Models/AgendamentoDAO.cs
@@ -6,11 +6,35 @@ namespace AppExemplo.Models
     public class AgendamentoDAO
     {
 
-    private readonly Conexao _conexao;
+        private readonly Conexao _conexao;
 
-    public AgendamentoDAO(Conexao conexao)
-    {
-        _conexao = conexao;
-    }
+        public AgendamentoDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+
+        public List<Agendamento> ListarTodos()
+        {
+            var lista = new List<Agendamento>();
+
+            // Cria o comando SQL para buscar todos os cargos
+            var comando = _conexao.CreateCommand("SELECT * FROM Agendamento");
+            var leitor = comando.ExecuteReader();
+
+            while (leitor.Read())
+            {
+                var agendamento = new Agendamento
+                {
+                    Id = leitor.GetInt32("id_age"),
+                    Data = leitor.GetDateTime("data_age"),
+                    Horario = leitor.GetString("horario_age"),
+                    IdCliente = leitor.GetInt32("id_cli_fk")
+                };
+
+                lista.Add(agendamento);
+            }
+
+            return lista;
+        }
     }
 }
